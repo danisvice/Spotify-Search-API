a37e5fbb2dce49f6c5239b413fe6b003ba9834f8
Danny <d.ortixe@icloud.com>
2023-08-16 17:23:23 -0700

    Updated; skeleton added, async main added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use reqwest::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
+use serde::{Deserialize, Serialize};
+use std::env;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ExternalUrls{
+    spotify: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Artist{
+    name: String,
+    external_urls: ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Album {
+    name: String,
+    artists: Vec<Artist>,
+    external_urls: ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Song {
+    name: String,
+    href: String,
+    popularity: u32,
+    album: Album,
+    external_urls: ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct APIResponse{
+    songs: Items<Song>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Items<T>{
+    items: Vec<T>
+}
+
+fn print_songs(songs: vec<&Song){
+
+}
+
+#[tokio::main]
+async fn main(){
+    let args: Vec<String> = env::args().collect();
+    let search_query = &args[1];
+    let auth_token = &args[2];
+    let url = format!(
+        "https://api.spotify.com/v1/search?q={query}&type=track,artist",
+        query = search_query
+    );
 }
